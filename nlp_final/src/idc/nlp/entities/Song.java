19825d97f5272d28cabc05120a6891ded0eb54fd
zivl <levi.ziv@gmail.com>
2013-07-02 17:31:05 +0300

    fix in rap songs

--- nlp_final/src/idc/nlp/entities/Song.java
+++ nlp_final/src/idc/nlp/entities/Song.java
@@ -1,11 +1,9 @@
 package idc.nlp.entities;
 
-import idc.nlp.parsers.ParseMode;
 import idc.nlp.utils.CountMap;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import de.bwaldvogel.liblinear.FeatureNode;
 
@@ -15,7 +13,7 @@ public class Song {
 	private CountMap<String> generalLyricsCount;
 	protected FeatureNode[] featureNodes;
 	private List<List<String>> lyrics;
-	public static int ADDITIONAL_FEATURES_AMOUNT = 1;
+	public static int ADDITIONAL_FEATURES_AMOUNT = 4;
 
 	public Song(CountMap<String> lyricsCountMap, List<List<String>> lines) {
 		this.lyricsCount = lyricsCountMap;
@@ -34,22 +32,23 @@ public class Song {
 	public FeatureNode[] convertToFeatureNodes() {
 		int[] tempArr = new int[lyricsCount.size()];
 		int i = 0;
-		
+
 		for (String word : lyricsCount.keySet()) {
 			tempArr[i] = LyricsData.getId(word);
 			i++;
 		}
-		
+
 		Arrays.sort(tempArr);
 		FeatureNode[] featureNodes = new FeatureNode[tempArr.length + ADDITIONAL_FEATURES_AMOUNT];
 		for (i = 0; i < tempArr.length; i++) {
 			featureNodes[i] = new FeatureNode(tempArr[i], lyricsCount.get(LyricsData.getWord(tempArr[i])));
 		}
-		
-		for(FeatureNode feature : getAdditionalFeatures())
-		{
-			featureNodes[i] = feature; 
-			i++;
+
+		if (ADDITIONAL_FEATURES_AMOUNT > 0) {
+			for (FeatureNode feature : getAdditionalFeatures()) {
+				featureNodes[i] = feature;
+				i++;
+			}
 		}
 		return featureNodes;
 	}
@@ -58,70 +57,58 @@ public class Song {
 	public String toString() {
 		return this.getLyrics().toString();
 	}
-	
-	private FeatureNode[] getAdditionalFeatures()
-	{
+
+	private FeatureNode[] getAdditionalFeatures() {
 		FeatureNode[] result = new FeatureNode[ADDITIONAL_FEATURES_AMOUNT];
 		int totalLength = computeLength();
-		//result[0] = new FeatureNode(LyricsData.size() + 1, averageLyricsOriginality());
-		//result[1] =  new FeatureNode(LyricsData.size() + 2, computeAverageWordAppearance(totalLength));
-		//result[2] =  new FeatureNode(LyricsData.size() + 3, computeAverageLineLength(totalLength));
-		//result[3] =  new FeatureNode(LyricsData.size() + 4, wordDiversity(totalLength));
+		result[0] = new FeatureNode(LyricsData.size() + 1, averageLyricsOriginality());
+		result[1] = new FeatureNode(LyricsData.size() + 2, computeAverageWordAppearance());
+		result[2] =  new FeatureNode(LyricsData.size() + 3, computeAverageLineLength(totalLength));
+		result[3] =  new FeatureNode(LyricsData.size() + 4, wordDiversity(totalLength));
 		return result;
 	}
-	
-	private int computeLength()
-	{
+
+	private int computeLength() {
 		int length = 0;
-		for(List<String> line : lyrics)
-		{
+		for (List<String> line : lyrics) {
 			length += line.size();
 		}
-		
+
 		return length;
 	}
-	
-	private double computeAverageLineLength(double totalLength)
-	{
+
+	private double computeAverageLineLength(double totalLength) {
 		return totalLength / lyrics.size();
 	}
-	
-	private double computeAverageWordAppearance()
-	{
+
+	private double computeAverageWordAppearance() {
 		double variety = 0;
-		for(String word : generalLyricsCount.keySet())
-		{
+		for (String word : generalLyricsCount.keySet()) {
 			variety += generalLyricsCount.get(word);
 		}
 		return variety / generalLyricsCount.size();
 	}
-	
-	private double wordDiversity(double totalWords)
-	{
+
+	private double wordDiversity(double totalWords) {
 		return totalWords / generalLyricsCount.size();
 	}
-	
-	private static CountMap<String> generateLyricsCountList(List<List<String>> lyrics)
-	{
+
+	private static CountMap<String> generateLyricsCountList(List<List<String>> lyrics) {
 		CountMap<String> lyricsCount = new CountMap<String>();
-		for(List<String> line : lyrics)
-		{
-			for(String word : line) 
-			{
+		for (List<String> line : lyrics) {
+			for (String word : line) {
 				lyricsCount.increment(word);
 			}
 		}
 		return lyricsCount;
 	}
-	
-	private double averageLyricsOriginality()
-	{
+
+	private double averageLyricsOriginality() {
 		double result = 0.0;
-		for(String word : lyricsCount.keySet())
-		{
+		for (String word : lyricsCount.keySet()) {
 			result += LyricsData.wordAmount(word);
 		}
-		
+
 		return result / lyricsCount.size();
 	}
 }
--- nlp_final/src/idc/nlp/main/Trainer.java
+++ nlp_final/src/idc/nlp/main/Trainer.java
@@ -9,7 +9,7 @@ import idc.nlp.models.SongClassifierModel;
 public class Trainer {
 
 	final static double constraints = 1.0;
-	final static String modelFilename = "3_genre_model_c_" + constraints + "_"
+	final static String modelFilename = "5_genre_model_c_" + constraints + "_"
 			+ new SimpleDateFormat("dd_MM_yy_HH_mm", Locale.ENGLISH).format(new Date()) + ".llm";
 
 	public static void main(String[] args) {
@@ -17,6 +17,6 @@ public class Trainer {
 		SongClassifierModel model = new SongClassifierModel(constraints);
 		model.train(modelFilename);
 		long elapsedTime = System.currentTimeMillis() - startTime;
-		System.out.println("Training time: " + (elapsedTime / 1000) + " seconds");
+		System.out.println("Training time: " + (elapsedTime / 1000.0) + " seconds");
 	}
 }
